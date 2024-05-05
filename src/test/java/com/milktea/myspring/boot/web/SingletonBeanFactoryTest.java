547badc9a0950e469ae64d9d392d4c1c66920cea
MilkTea24 <kimmanjae15@pusan.ac.kr>
2024-05-05 11:48:50 +0900

    fix: SingletonBeanFactoryTest 수정

--- src/test/java/com/milktea/myspring/boot/web/SingletonBeanFactoryTest.java
+++ src/test/java/com/milktea/myspring/boot/web/SingletonBeanFactoryTest.java
@@ -1,6 +1,5 @@
 package com.milktea.myspring.boot.web;
 
-import com.milktea.myspring.annotations.Autowired;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -32,7 +31,7 @@ public class SingletonBeanFactoryTest {
         singletonBeanFactory.createBeans();
 
         //then
-        DefaultSingletonBeanRegistry instanceRegistry = singletonBeanFactory.getInstanceRegistry();
+        BeanRegistry instanceRegistry = singletonBeanFactory.getBeanRegistry();
         Assertions.assertNotNull(instanceRegistry.getSingleton(ClassList.A.class));
         Assertions.assertNotNull(instanceRegistry.getSingleton(ClassList.B.class));
         Assertions.assertNotNull(instanceRegistry.getSingleton(ClassList.C.class));
@@ -57,7 +56,7 @@ public class SingletonBeanFactoryTest {
         singletonBeanFactory.createBeans();
 
         //then
-        DefaultSingletonBeanRegistry instanceRegistry = singletonBeanFactory.getInstanceRegistry();
+        BeanRegistry instanceRegistry = singletonBeanFactory.getBeanRegistry();
         Assertions.assertNotNull(instanceRegistry.getSingleton(ClassList.D.class));
         Assertions.assertNotNull(instanceRegistry.getSingleton(ClassList.E.class));
         Assertions.assertNotNull(instanceRegistry.getSingleton(ClassList.F.class));
@@ -83,7 +82,7 @@ public class SingletonBeanFactoryTest {
         singletonBeanFactory.createBeans();
 
         //then
-        DefaultSingletonBeanRegistry instanceRegistry = singletonBeanFactory.getInstanceRegistry();
+        BeanRegistry instanceRegistry = singletonBeanFactory.getBeanRegistry();
         ClassList.D instanceD = (ClassList.D) instanceRegistry.getSingleton(ClassList.D.class);
         Assertions.assertNotNull(instanceD.e);
         Assertions.assertNotNull(instanceD.f);
