954d0d5f50f0d40dc0e949e3d3aca475f87f9883
MilkTea24 <kimmanjae15@pusan.ac.kr>
2024-05-08 11:46:00 +0900

    fix: PathVariableMethodArgumentResolver 코드 수정

--- src/main/java/com/milktea/myspring/boot/web/servlet/PathVariableMethodArgumentResolver.java
+++ src/main/java/com/milktea/myspring/boot/web/servlet/PathVariableMethodArgumentResolver.java
@@ -40,37 +40,4 @@ public class PathVariableMethodArgumentResolver implements HandlerMethodArgument
 
         return conversionService.convert(value, parameter.getType());
     }
-
-    //json 또는 pathVariable로 전달된 데이터를 추출해야함
-    //params는 pathVariable 포함해서 입력받은 key Value를 모두 저장해놓은 맵
-    /*
-    public Object[] extractArgsForMethod(Method handler, Map<String, Object> params) {
-        if (!pathVariableFlag) getHandler(request);
-
-        List<Object> args = new ArrayList<>();
-        Parameter[] parameters = handler.getParameters();
-        Class<?> clazz = handler.getDeclaringClass();
-
-        SimplePathPatternParser
-
-        return args.toArray();
-    }
-
-    //"/students/{id}"와 같은 PathVariable을 구현해야 함
-    public Map<String, Object> extractPathVariables(HttpServletRequest request) {
-        if (!pathVariableFlag && getHandler(request) == null) {
-            throw new RuntimeException("Request에 대응하는 핸들러를 찾을 수 없습니다 : " + request.getMethod() + " " + request.getRequestURI());
-        }
-
-        return pathVariables;
-    }
-
-    public Object getControllerInstance(HttpServletRequest request) {
-        if (controllerClass == null && getHandler(request) == null){
-            throw new RuntimeException("Request에 대응하는 핸들러를 찾을 수 없습니다 : " + request.getMethod() + " " + request.getRequestURI());
-        }
-
-        return controllerRegistry.getControllerBeans().get(controllerClass);
-    }
-     */
 }
